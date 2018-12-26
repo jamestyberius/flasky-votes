c174c69f71640885e4c5d4f3dc75c397d8419217
JamesTyberius <jamestyberius1@gmail.com>
2018-12-26 14:09:48 -0500

    templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username': 'Tyberius'}
+    posts = [
+        {
+            'author': {'username': 'Dax'},
+            'body': 'Loaded VA-07 data'
+        },
+        {
+            'author': {'username': 'Picard'},
+            'body': 'Approved VA-07 data'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
